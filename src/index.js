aa7efd84bf970da4719dc51bdae2fbc4296d4c2e
cmedwar <christopher.edwards@caresource.com>
2023-04-15 12:14:10 -0400

    Added default index file to easily import specific functionality from the package from one consolidated import

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/* index.js
+    
+    Main library index.
+*/
+
+export { default as DataMap } from "#lib/class/data-map";
+export { default as LinkedList } from "#lib/class/linked-list";
+
+export { default as ready } from "#lib/dom/ready";
+
+export { default as clamp } from "#lib/utils/clamp";
+export { default as deepCopy } from "#lib/utils/deep-copy";
+export { default as dispatch } from "#lib/utils/dispatch";
+export { default as immutable } from "#lib/utils/immutable";
